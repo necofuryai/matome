15446c1d78a4b04b6a81a7ccd471c59382551254
necofuryai <haruneco210@gmail.com>
2020-10-03 18:09:33 +0900

    公式サイト初期コミット

---
+++ public/js/analytics.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDOAm-uKc3JldJjCf74h-dD5JZjrN7U8ek",
+    authDomain: "hinami-8b5a3.firebaseapp.com",
+    databaseURL: "https://hinami-8b5a3.firebaseio.com",
+    projectId: "hinami-8b5a3",
+    storageBucket: "hinami-8b5a3.appspot.com",
+    messagingSenderId: "888357496712",
+    appId: "1:888357496712:web:9237553e18ff851c17bc3d",
+    measurementId: "G-92SBCGMLFW"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
